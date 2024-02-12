28cf78a5fef52f15162d0be2b98cd34c16d5bd63
JoaoOliveiraZz <linajoaovictor@gmail.com>
2024-02-12 16:47:01 -0300

    Searching notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
 import NlwLogo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/newNoteCard";
 import { NoteCard } from "./components/noteCard";
@@ -17,6 +17,7 @@ export function App() {
 
     return (notesOnStorage) ? JSON.parse(notesOnStorage) : []
   })
+  const [search, setSearch] = useState('')
 
   function setNotesOnSave(content: string){
     const newNote: Notes = {
@@ -32,6 +33,17 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ''
+    ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()))
+    : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={NlwLogo} alt="Nlw Expert" />
@@ -40,6 +52,7 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas..."
           className=" bg-transparent w-full text-3xl outline-none text-slate-500 font-semibold tracking-tight placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
@@ -50,7 +63,7 @@ export function App() {
       <NewNoteCard setNotesOnSave={setNotesOnSave} />
 
       {
-        notes.map(note => {
+        filteredNotes.map(note => {
           return (
             <NoteCard key={note.id} note={note} />
           )
